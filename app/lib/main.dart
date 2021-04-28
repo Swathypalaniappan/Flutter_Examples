737936e65baf999046d6a21d06932c72e108563f
swathy <swathypalani2000@gmail.com>
2021-04-28 19:06:29 +0530

    appBar,container

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,23 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(Home());
 }
 
-class MyApp extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title:"flutter first", 
-      home:HomePage(),
-      debugShowCheckedModeBanner:false,
+      home: FirstPage(),
+      title: "appbar",
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatelessWidget{
+class FirstPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor:Colors.blue,
+            title:Text("AppBar"),
+            titleTextStyle: TextStyle(fontSize:40),
+            leading:Icon(Icons.arrow_back_ios_sharp),
+            leadingWidth:100,
+            centerTitle: true,
+            elevation:10,
+            toolbarOpacity:0.7,
+            toolbarHeight:200,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            titleSpacing:10.0,
+            shadowColor: Colors.redAccent,
+          ),
+      body: Center(
+        child: Container(
+          // width: 300,
+          // height: 300,
+          // child: Text('Boxshadow'),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              // border: Border.all(color: Colors.black, width: 2.0),
+              // shape: BoxShape.circle,
+              // color:Colors.amber,
+              gradient: LinearGradient(
+                colors:<Color>[Colors.deepOrangeAccent,Colors.yellowAccent,Colors.deepOrange],
+                begin:Alignment.topLeft,
+                end:Alignment.bottomRight,
+              ),
+              // boxShadow: <BoxShadow>[
+              //   BoxShadow(
+              //       blurRadius: 2.5,
+              //       color: Colors.redAccent,
+              //       spreadRadius: 10,
+              //       offset: Offset(5, 10)),
+              //   BoxShadow(
+              //       blurRadius: 2.5,
+              //       color: Colors.blueAccent,
+              //       spreadRadius: 10,
+              //       offset: Offset(-10, -5))
+              // ]
+            ),
+        ),
+      ),
+    ));
   }
 }
