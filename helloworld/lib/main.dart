4d8bb8f2e4501c1e0bf925a21781d244cff41d37
swathy <swathypalani2000@gmail.com>
2021-04-28 19:12:39 +0530

    types of gradient

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -1,6 +1,6 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(Home());
 }
 
@@ -8,36 +8,47 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-      title: "Text Widget",
-      debugShowCheckedModeBanner: false,
+      home:First(),
+      title:"gradient",
+      debugShowCheckedModeBanner:false,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+// class First extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         alignment:Alignment.center,
+//         decoration:BoxDecoration(
+//           gradient:RadialGradient(
+//             colors: <Color>[Colors.deepOrangeAccent,Colors.yellowAccent,Colors.deepOrange,Colors.yellow],
+//             //center:,
+//             focalRadius:10.0,
+//             //focal:Alignment.center,
+//             radius:1,
+//          ),  
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class First extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello World is the first program to be made in any programming language like c,c++,java and python etc flutter is a ui development kit developed by google and it uses dart language",
-      //maxLines:1,
-      //overflow:TextOverflow.ellipsis,
-      //textAlign:TextAlign.right,
-      textDirection:TextDirection.ltr,
-
-          style: TextStyle(
-              color: Colors.blue,
-              fontStyle:FontStyle.italic,
-              decoration:TextDecoration.underline,
-              fontSize: 50.0,
-              fontWeight: FontWeight.bold,
-              //backgroundColor:Colors.yellow,
-              letterSpacing:5.0,
-              wordSpacing:10.0,
-              //height:10.0,
-              shadows: <Shadow>[
-                Shadow(blurRadius: 5.0, color: Colors.green[700],offset:Offset(5.0,5.0))
-              ])),
+      body:Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: SweepGradient(
+            colors:<Color>[Colors.purpleAccent,Colors.lightGreenAccent,Colors.pinkAccent],
+            startAngle: 0.0,
+            endAngle:1.0,
+          ),
+        ),
+      ),
     );
   }
 }
