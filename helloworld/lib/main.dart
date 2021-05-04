4013b48e13aba3338aff4390f8ff7fc8510a08a7
swathy <swathypalani2000@gmail.com>
2021-05-04 13:23:09 +0530

    Buttons

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -8,45 +8,83 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:First(),
-      title:"gradient",
+      home:FirstApp(),
       debugShowCheckedModeBanner:false,
+      title:"Buttons",
     );
   }
 }
 
-// class First extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         alignment:Alignment.center,
-//         decoration:BoxDecoration(
-//           gradient:RadialGradient(
-//             colors: <Color>[Colors.deepOrangeAccent,Colors.yellowAccent,Colors.deepOrange,Colors.yellow],
-//             //center:,
-//             focalRadius:10.0,
-//             //focal:Alignment.center,
-//             radius:1,
-//          ),  
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-class First extends StatelessWidget {
+class FirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          gradient: SweepGradient(
-            colors:<Color>[Colors.purpleAccent,Colors.lightGreenAccent,Colors.pinkAccent],
-            startAngle: 0.0,
-            endAngle:1.0,
-          ),
+      body:SizedBox.expand(
+        child:Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children:<Widget>[
+            SizedBox(
+              height:50,
+              width:300,
+              child:ElevatedButton(
+              onPressed:(){},
+              child:Text("Elevated Button"),
+              style: ButtonStyle(
+                //backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreenAccent),
+                alignment:Alignment.centerLeft,
+                shape:MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  //side:BorderSide(color:Colors.redAccent,width:2.0),
+                )),
+                padding:MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.all(30)),
+
+              ),
+            )),
+            SizedBox(
+              height:20,
+            ),
+            SizedBox(
+              height:50,
+              width:300,
+              child:TextButton(
+                onPressed:(){},
+                child:Text("Text Button"),
+                style: ButtonStyle(
+                  //backgroundColor: MaterialStateProperty.all<Color>(Colors.pinkAccent),
+                  shape:MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                    borderRadius:BorderRadius.circular(20),
+                  )),
+                  side: MaterialStateProperty.all<BorderSide>(BorderSide(
+                    //color:Colors.amberAccent,
+                    width:3.0
+                  )),
+                  alignment:Alignment.centerRight,
+                  padding:MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.only(right:20)),
+
+                ),
+              ),
+            ),
+            SizedBox(
+              height:20,
+            ),
+            SizedBox(
+              height:50,
+              width:300,
+              child:OutlinedButton(
+                onPressed:(){},
+                child:Text("Outlined Button"),
+                style:ButtonStyle(
+                  //backgroundColor:MaterialStateProperty.all<Color>(Colors.deepOrangeAccent),
+                  alignment:Alignment.center,
+                  side:MaterialStateProperty.all<BorderSide>(BorderSide(
+                    //color:Colors.blueGrey,
+                    width:4.0,
+                  )),
+                  elevation:MaterialStateProperty.all<double>(40),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
